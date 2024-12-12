fee16619b2a99cf8beeecf6783315a241bc1135c
yuma <yuma.fuu05@gmail.com>
2024-12-12 18:54:43 +0900

    [fix] typo

--- config.ts
+++ config.ts
@@ -7,8 +7,8 @@ type Config = {
   dirs: string[];
 };
 
-const DEFAULT_CONFIG = () => `${Bun.env.HOME}/.config/ghq-fzf/config.yaml`;
-const configFilePath = () => Bun.env.GHQ_FZF_CONFIG || DEFAULT_CONFIG;
+const DEFAULT_CONFIG = `${Bun.env.HOME}/.config/ghq-fzf/config.yaml`;
+const configFilePath = Bun.env.GHQ_FZF_CONFIG || DEFAULT_CONFIG;
 const configFile = await Bun.file(configFilePath).text();
 const config: Config = parse(configFile);
 
